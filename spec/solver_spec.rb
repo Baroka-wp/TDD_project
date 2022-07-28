6df02ce0bbe2940e66f32425984b4d0c7558b1c9
baroka <birotori@gmail.com>
2022-07-28 11:04:53 +0100

    Test for revese method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,19 +5,29 @@ describe 'Solver' do
   before(:each) do
     @solver = Solver.new
   end
-  it 'should return 120 for factorial 5' do
-    expect(@solver.factorial(5)).to eq 120
+  context '#factorial' do
+    it 'should return 120 for factorial 5' do
+      expect(@solver.factorial(5)).to eq 120
+    end
+    it 'should return 1 for factorial 0' do
+      expect(@solver.factorial(0)).to eq 1
+    end
+    it 'should return 1 for factorial 1' do
+      expect(@solver.factorial(1)).to eq 1
+    end
+    it 'should return 2 for factorial 2' do
+      expect(@solver.factorial(2)).to eq 2
+    end
+    it 'should return Error for factorial -1' do
+      expect(@solver.factorial(-1)).to eq 'Error'
+    end
   end
-  it 'should return 1 for factorial 0' do
-    expect(@solver.factorial(0)).to eq 1
-  end
-  it 'should return 1 for factorial 1' do
-    expect(@solver.factorial(1)).to eq 1
-  end
-  it 'should return 2 for factorial 2' do
-    expect(@solver.factorial(2)).to eq 2
-  end
-  it 'should return Error for factorial -1' do
-    expect(@solver.factorial(-1)).to eq 'Error'
+  context 'Reverse' do
+    it 'should return "olleh" for "hello"' do
+      expect(@solver.reverse('hello')).to eq 'olleh'
+    end
+    it 'should return "Error" for "Error"' do
+      expect(@solver.reverse(1)).to eq 'Error'
+    end
   end
 end
